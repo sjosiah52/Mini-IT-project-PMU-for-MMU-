377b6fe61a62922d179c276b9ad1159270ef9e6d
sjosiah52 <167731716+sjosiah52@users.noreply.github.com>
2024-05-19 13:40:21 +0800

    Update First_page.py
    Updating the software's user interface size.

--- MiniIT/First_page.py
+++ MiniIT/First_page.py
@@ -5,6 +5,25 @@ import user_page
 
 customtkinter.set_appearance_mode("light")
 customtkinter.set_default_color_theme("dark-blue")
+def fullscreen(window):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    if screen_width / screen_height > 16 / 9:
+        height = screen_height
+        width = int(height * 16 / 9)
+    else:
+        width = screen_width
+        height = int(width * 9 / 16)
+
+    window.geometry(f"{width}x{height}")
+    window.attributes('-fullscreen', True)
+
+root = customtkinter.CTk()  # Only create the CTk window
+root.title("Full Screen 16:9 Window")
+root.bind("<Escape>", lambda e: root.attributes('-fullscreen', False))
+
+fullscreen(root)
 
 root = customtkinter.CTk()
 root = tkinter.Tk()
