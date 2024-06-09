8ac9a57f8d3637838b3bddc360aa15c788ed8d0d
sjosiah52 <167731716+sjosiah52@users.noreply.github.com>
2024-06-10 01:55:49 +0800

    Create init_db.py
    Using sqlite as back-end for database

---
+++ MiniIT/init_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def init_db():
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            mmu_id TEXT NOT NULL UNIQUE,
+            name TEXT NOT NULL,
+            phone TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+        """)
+        conn.commit()
+
+if __name__ == "__main__":
+    init_db()
